9dba96a641406eb1fea51cc0e5b196710df66c60
Dung <phamtiendung2k4@gmail.com>
2025-09-11 19:22:00 +0700

    update home page

--- admin/index.php
+++ admin/index.php
@@ -92,16 +92,16 @@ if (!isset($_SESSION['user_id']) || empty($_SESSION['is_admin'])) {
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
+                    <span>Cài đặt sản phẩm</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
+                        <h6 class="collapse-header">Cài đặt sản phẩm:</h6>
+                        <a class="collapse-item" href="">Sản phẩm</a>
+                        <a class="collapse-item" href="">Đường viền</a>
+                        <a class="collapse-item" href="">Hoạt hình</a>
+                        <a class="collapse-item" href="">Khác</a>
                     </div>
                 </div>
             </li>
